4621b7d3d35f646a3a879b900c81bd741d706cbf
xichuanwushao <avatarwx@163.com>
2021-07-26 20:22:33 +0800

    3-6 模糊查询 参数使用实体进行模糊查询 并返回指定类型实体

--- src/main/java/com/xichuan/wiki/controller/EbookController.java
+++ src/main/java/com/xichuan/wiki/controller/EbookController.java
@@ -3,6 +3,7 @@ package com.xichuan.wiki.controller;
 import com.xichuan.wiki.domain.Ebook;
 import com.xichuan.wiki.req.EbookReq;
 import com.xichuan.wiki.resp.CommonResp;
+import com.xichuan.wiki.resp.EbookResp;
 import com.xichuan.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -48,6 +49,19 @@ public class EbookController {
     public CommonResp list(EbookReq ebookReq) {
         CommonResp<List<Ebook>> resp = new CommonResp<>();
         List<Ebook> list = ebookService.list( ebookReq);
+        resp.setContent(list);
+        return resp;
+    }
+
+    /***
+     * 参数使用实体进行模糊查询 并返回指定类型实体
+     * @param ebookReq
+     * @return
+     */
+    @GetMapping("/listResp")
+    public CommonResp listResp(EbookReq ebookReq) {
+        CommonResp<List<EbookResp>> resp = new CommonResp<>();
+        List<EbookResp> list = ebookService.listResp( ebookReq);
         resp.setContent(list);
         return resp;
     }
--- src/main/java/com/xichuan/wiki/service/EbookService.java
+++ src/main/java/com/xichuan/wiki/service/EbookService.java
@@ -4,9 +4,12 @@ import com.xichuan.wiki.domain.Ebook;
 import com.xichuan.wiki.domain.EbookExample;
 import com.xichuan.wiki.mapper.EbookMapper;
 import com.xichuan.wiki.req.EbookReq;
+import com.xichuan.wiki.resp.EbookResp;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -30,6 +33,21 @@ public class EbookService {
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         criteria.andNameLike("%"+ebookReq.getName()+"%");
         return ebookMapper.selectByExample(ebookExample);
+    }
+
+    public List<EbookResp> listResp(EbookReq ebookReq) {
+        EbookExample ebookExample = new EbookExample();
+        EbookExample.Criteria criteria = ebookExample.createCriteria();
+        criteria.andNameLike("%"+ebookReq.getName()+"%");
+        List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+
+        List<EbookResp> respList = new ArrayList<>() ;
+        for (Ebook ebook :ebookList){
+            EbookResp ebookResp = new EbookResp();
+            BeanUtils.copyProperties(ebook,ebookResp);
+            respList.add(ebookResp);
+        }
+        return respList;
     }
 
 }
