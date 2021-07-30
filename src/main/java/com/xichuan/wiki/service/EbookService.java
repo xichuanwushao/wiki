96a3afc6bc893e778eb7b2697c3616f73aecf86f
xichuanwushao <avatarwx@163.com>
2021-07-30 18:37:01 +0800

    6-4 使用PageHelper实现后端分页 PageHelper下一句只能一个select，打印pageinfo信息

--- src/main/java/com/xichuan/wiki/service/EbookService.java
+++ src/main/java/com/xichuan/wiki/service/EbookService.java
@@ -1,12 +1,15 @@
 package com.xichuan.wiki.service;
 
 import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.xichuan.wiki.domain.Ebook;
 import com.xichuan.wiki.domain.EbookExample;
 import com.xichuan.wiki.mapper.EbookMapper;
 import com.xichuan.wiki.req.EbookReq;
 import com.xichuan.wiki.resp.EbookResp;
 import com.xichuan.wiki.util.CopyUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
@@ -19,6 +22,8 @@ import java.util.List;
 public class EbookService {
     @Resource
     private EbookMapper ebookMapper;
+
+    private static final Logger log = LoggerFactory.getLogger(EbookService.class);
 
     public List<Ebook> list() {
         return ebookMapper.selectByExample(null);
@@ -83,13 +88,17 @@ public class EbookService {
      * @return
      */
     public List<EbookResp> listResp2(EbookReq ebookReq) {
-        PageHelper.startPage(1,2);
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         if(!ObjectUtils.isEmpty(ebookReq.getName())){
             criteria.andNameLike("%"+ebookReq.getName()+"%");
         }
+        PageHelper.startPage(1,2);
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
+        log.info("总行数:{}",pageInfo.getTotal());
+        log.info("总页数:{}",pageInfo.getPages());
         List<EbookResp> respList = CopyUtil.copyList(ebookList, EbookResp.class);
         return respList;
     }
