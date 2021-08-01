c522e41376bf948b1b462f5087e0788fe2ff346f
xichuanwushao <avatarwx@163.com>
2021-08-01 12:25:47 +0800

    6-7 制作电子书表单 模态框中显示电子表单，删除多余示例代码，编辑某一行时，可以将该行数据显示到表单

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -34,57 +34,30 @@
           :confirm-loading="modalLoading"
           @ok="handleModalOk"
   >
-    <a-form :model="formState" :label-col="labelCol" :wrapper-col="wrapperCol">
-      <a-form-item label="Activity name">
-        <a-input v-model:value="formState.name" />
+    <a-form :model="ebook" :label-col="{span:6 }" :wrapper-col="{ span: 18 }">
+      <a-form-item label="封面">
+        <a-input v-model:value="ebook.cover" />
       </a-form-item>
-      <a-form-item label="Activity zone">
-        <a-select v-model:value="formState.region" placeholder="please select your zone">
-          <a-select-option value="shanghai">Zone one</a-select-option>
-          <a-select-option value="beijing">Zone two</a-select-option>
-        </a-select>
+      <a-form-item label="名称">
+        <a-input v-model:value="ebook.name" />
       </a-form-item>
-      <a-form-item label="Activity time">
-        <a-date-picker
-                v-model:value="formState.date1"
-                show-time
-                type="date"
-                placeholder="Pick a date"
-                style="width: 100%"
-        />
+      <a-form-item label="分类一">
+        <a-input v-model:value="ebook.category1Id" />
       </a-form-item>
-      <a-form-item label="Instant delivery">
-        <a-switch v-model:checked="formState.delivery" />
+      <a-form-item label="分类二">
+        <a-input v-model:value="ebook.category2Id" />
       </a-form-item>
-      <a-form-item label="Activity type">
-        <a-checkbox-group v-model:value="formState.type">
-          <a-checkbox value="1" name="type">Online</a-checkbox>
-          <a-checkbox value="2" name="type">Promotion</a-checkbox>
-          <a-checkbox value="3" name="type">Offline</a-checkbox>
-        </a-checkbox-group>
+      <a-form-item label="描述">
+        <a-input v-model:value="ebook.desc"  type="textarea" />
       </a-form-item>
-      <a-form-item label="Resources">
-        <a-radio-group v-model:value="formState.resource">
-          <a-radio value="1">Sponsor</a-radio>
-          <a-radio value="2">Venue</a-radio>
-        </a-radio-group>
-      </a-form-item>
-      <a-form-item label="Activity form">
-        <a-input v-model:value="formState.desc" type="textarea" />
-      </a-form-item>
-      <a-form-item :wrapper-col="{ span: 14, offset: 4 }">
-        <a-button type="primary" @click="onSubmit">Create</a-button>
-        <a-button style="margin-left: 10px">Cancel</a-button>
-      </a-form-item>
+
     </a-form>
   </a-modal>
 </template>
 
 <script lang="ts">
-  import { defineComponent, onMounted, ref , reactive, toRaw, UnwrapRef } from 'vue';
+  import { defineComponent, onMounted, ref  } from 'vue';
   import axios from 'axios';
-  import { Moment } from 'moment';
-
 
   export default defineComponent({
     name: 'AdminEbook',
@@ -167,6 +140,7 @@
         });
       };
 
+      const ebook = ref();
       const modalText = ref<string>('Content of the modal');
       const modalVisible = ref<boolean>(false);
       const modalLoading = ref<boolean>(false);
@@ -179,8 +153,9 @@
         }, 2000);
       };
 
-      const edit = () => {
+      const edit = (record : any) => {
         modalVisible.value = true;
+        ebook.value = record;
       };
 
       onMounted(() => {
@@ -190,28 +165,6 @@
         });
       });
 
-      interface FormState {
-        name: string;
-        region: string | undefined;
-        date1: Moment | undefined;
-        delivery: boolean;
-        type: string[];
-        resource: string;
-        desc: string;
-      }
-
-      const formState: UnwrapRef<FormState> = reactive({
-        name: '',
-        region: undefined,
-        date1: undefined,
-        delivery: false,
-        type: [],
-        resource: '',
-        desc: '',
-      });
-      const onSubmit = () => {
-        console.log('submit!', toRaw(formState));
-      };
 
       return {
         ebooks,
@@ -220,16 +173,13 @@
         loading,
         handleTableChange,
 
-        modalText,
+        ebook,
         modalVisible,
         modalLoading,
         edit,
-        handleModalOk,
 
-        labelCol: { span: 4 },
-        wrapperCol: { span: 14 },
-        formState,
-        onSubmit,
+
+        handleModalOk,
       }
     }
   });
