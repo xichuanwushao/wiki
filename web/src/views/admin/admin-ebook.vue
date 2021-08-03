8a3ef67fa236857beeda0e64f3df3fceba1b2a42
xichuanwushao <avatarwx@163.com>
2021-08-03 11:41:56 +0800

    6-11 集成Validation做参数校验 1.增加全局异常统一处理类 2.前端页面返回错误提示

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -71,6 +71,7 @@
 <script lang="ts">
   import { defineComponent, onMounted, ref  } from 'vue';
   import axios from 'axios';
+  import {message} from "ant-design-vue";
 
   export default defineComponent({
     name: 'AdminEbook',
@@ -135,10 +136,14 @@
         }).then((response) => {
           loading.value = false;
           const data = response.data;
-          ebooks.value = data.content.list;
-          //重置分页按钮
-          pagination.value.current = params.page;
-          pagination.value.total = data.content.total;
+          if(data.success){
+            ebooks.value = data.content.list;
+            //重置分页按钮
+            pagination.value.current = params.page;
+            pagination.value.total = data.content.total;
+          }else {
+            message.error(data.message);
+          }
         });
       };
 
