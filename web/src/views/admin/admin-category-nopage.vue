76ac0e65d57f826463603981143f53b2da299b0f
xichuanwushao <avatarwx@163.com>
2021-08-05 12:31:15 +0800

    7-4 分类表格显示优化 1.分类表格按照树形结构展示 增加数组转树形数据的工具方法

--- web/src/views/admin/admin-category-nopage.vue
+++ web/src/views/admin/admin-category-nopage.vue
@@ -114,6 +114,20 @@
           slots: { customRender: 'action' }
         }
       ];
+      /**
+       * 一级分类树，children属性就是二级分类
+       * [{
+       *   id: "",
+       *   name: "",
+       *   children: [{
+       *     id: "",
+       *     name: "",
+       *   }]
+       * }]
+       */
+
+      const level1 = ref(); // 一级分类树，children属性就是二级分类
+      level1.value = [];
 
       /**
        * 数据查询
@@ -126,7 +140,11 @@
           const data = response.data;
           if(data.success){
             categorys.value = data.content;
+            console.log("原始数组：", categorys.value);
 
+            level1.value = [];
+            level1.value = tools.array2Tree(categorys.value, 0);
+            console.log("树形结构：", level1);
           }else {
             message.error(data.message);
           }
@@ -194,6 +212,7 @@
 
 
       return {
+        level1,
         categorys,
         columns,
         loading,
