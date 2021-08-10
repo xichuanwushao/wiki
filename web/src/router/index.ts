e347812fbe16b477c623d13ec0253ae2ecdc1836
xichuanwushao <avatarwx@163.com>
2021-08-10 14:12:43 +0800

    8-3 从分类管理复制出文档管理的前后端代码 1.遗留问题 编辑表单中 只能选择一级文档作为父文档

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -4,6 +4,7 @@ import About from '../views/about.vue'
 import AdminEbook from '../views/admin/admin-ebook.vue'
 import AdminCategory from '../views/admin/admin-category.vue'
 import AdminCategoryNoPage from '../views/admin/admin-category-nopage.vue'
+import AdminDoc from '../views/admin/admin-doc.vue'
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
@@ -30,6 +31,10 @@ const routes: Array<RouteRecordRaw> = [
     path: '/admin/admin-category-nopage',
     name: 'AdminCategoryNoPage',
     component: AdminCategoryNoPage
+  },{
+    path: '/admin/admin-doc',
+    name: 'AdminDoc',
+    component: AdminDoc
   },
 ]
 
---
+++ web/src/views/admin/admin-doc.vue
@@ -0,0 +1,243 @@
+<template>
+
+  <a-layout>
+    <a-layout-content
+            :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+    >
+      <p>
+        <a-form
+                layout="inline"
+                :model="param"
+        >
+          <a-form-item>
+            <a-button type="primary"  @click="handleQuery()"  >
+              查询
+            </a-button>
+          </a-form-item>
+
+          <a-form-item>
+            <a-button type="primary"  @click="add()"  >
+              新增
+            </a-button>
+          </a-form-item>
+        </a-form>
+      </p>
+      <a-table
+              :columns="columns"
+              :row-key="record => record.id"
+              :data-source="level1"
+              :pagination="false"
+              :loading="loading"
+      >
+        <template #cover="{ text: cover }">
+          <img v-if="cover" :src="cover" alt="avatar" />
+        </template>
+        <template v-slot:action="{ text, record }">
+          <a-space size="small">
+            <a-button type="primary"  @click="edit(record)" >
+                编辑
+            </a-button>
+            <a-popconfirm
+                    title="删除后不可恢复，确认删除?"
+                    ok-text="确认"
+                    cancel-text="取消"
+                    @confirm="handleDelete(record.id)"
+            >
+              <a-button type="danger">
+                删除
+              </a-button>
+            </a-popconfirm>
+
+          </a-space>
+        </template>
+      </a-table>
+    </a-layout-content>
+  </a-layout>
+  <a-modal
+          title="分类表单"
+          v-model:visible="modalVisible"
+          :confirm-loading="modalLoading"
+          @ok="handleModalOk"
+  >
+    <a-form :model="doc" :label-col="{span:6 }" :wrapper-col="{ span: 18 }">
+      <a-form-item label="名称">
+        <a-input v-model:value="doc.name" />
+      </a-form-item>
+
+      <a-form-item label="父分类">
+        <a-select
+                v-model:value="doc.parent"
+                ref="select"
+        >
+          <a-select-option value="0">无</a-select-option>
+          <a-select-option  v-for = "c in level1" :key="c.id"  :value="c.id" :disabled="doc.id === c.id">
+                {{c.name}}
+          </a-select-option>
+
+        </a-select>
+      </a-form-item>
+
+      <a-form-item label="顺序">
+        <a-input v-model:value="doc.sort" />
+      </a-form-item>
+
+    </a-form>
+  </a-modal>
+</template>
+
+<script lang="ts">
+  import { defineComponent, onMounted, ref  } from 'vue';
+  import axios from 'axios';
+  import {message} from "ant-design-vue";
+  import {Tool as tools} from "@/util/tool";
+
+
+  export default defineComponent({
+    name: 'AdminDoc',
+    setup() {
+      const param = ref();
+      param.value = {};
+      const docs = ref();
+
+      const loading = ref(false);
+
+      const columns = [
+
+
+
+        {
+          title: '名称',
+          dataIndex: 'name'
+        },{
+          title: '父分类',
+          key: 'parent',
+          dataIndex: 'parent'
+        },
+        {
+          title: '顺序',
+          dataIndex: 'sort'
+        },
+        {
+          title: 'Action',
+          key: 'action',
+          slots: { customRender: 'action' }
+        }
+      ];
+      /**
+       * 一级分类树，children属性就是二级分类
+       * [{
+       *   id: "",
+       *   name: "",
+       *   children: [{
+       *     id: "",
+       *     name: "",
+       *   }]
+       * }]
+       */
+
+      const level1 = ref(); // 一级分类树，children属性就是二级分类
+      level1.value = [];
+
+      /**
+       * 数据查询
+       **/
+      const handleQuery = () => {
+        loading.value = true;
+        // 如果不清空现有数据，则编辑保存重新加载数据后，再点编辑，则列表显示的还是编辑前的数据
+        axios.get("/doc/all").then((response) => {
+          loading.value = false;
+          const data = response.data;
+          if(data.success){
+            docs.value = data.content;
+            console.log("原始数组：", docs.value);
+
+            level1.value = [];
+            level1.value = tools.array2Tree(docs.value, 0);
+            console.log("树形结构：", level1);
+          }else {
+            message.error(data.message);
+          }
+        });
+      };
+
+
+
+      const doc = ref();
+      const modalText = ref<string>('Content of the modal');
+      const modalVisible = ref<boolean>(false);
+      const modalLoading = ref<boolean>(false);
+      const handleModalOk = () => {
+        // modalText.value = 'The modal will be closed after two seconds';
+        modalLoading.value = true;
+        // setTimeout(() => {
+        //   modalVisible.value = false;
+        //   modalLoading.value = false;
+        // }, 2000);
+
+        axios.post("/doc/save",doc.value).then((response) => {
+          modalLoading.value = false;//loading的效果只要后端有返回的时候就应该去除掉 而不是返回成功的时候再去除掉
+          const data = response.data;
+          if(data.success){
+            modalVisible.value = false;
+            handleQuery();
+          }else{
+              message.error(data.message);
+          }
+        });
+      };
+      /***
+       * 编辑
+       * @param record
+       */
+      const edit = (record : any) => {
+        modalVisible.value = true;
+        doc.value = tools.copy(record);
+      };
+      /***
+       * 新增
+       */
+      const add = () => {
+        modalVisible.value = true;
+        doc.value = {};
+      };
+
+      /***
+       * 删除
+       */
+      //const handleDelete = ( id : number ) => {
+      const handleDelete = (id: number) => {
+        console.log("删除ID "+id);
+        axios.delete("/doc/delete/"+id).then((response) => {
+          const data = response.data;
+          if(data.success){
+            handleQuery();
+          }
+        });
+      };
+
+      onMounted(() => {
+        handleQuery();
+      });
+
+
+      return {
+        level1,
+        //docs,
+        columns,
+        loading,
+        doc,
+
+        modalVisible,
+        modalLoading,
+        handleModalOk,
+
+        edit,
+        add,
+        handleDelete,
+
+        handleQuery,
+        param,
+      }
+    }
+  });
+</script>
