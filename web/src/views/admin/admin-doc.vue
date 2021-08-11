7ade2c5d32c83bec57c8ddad1d47ba1e2245feb7
xichuanwushao <avatarwx@163.com>
2021-08-11 16:34:46 +0800

    8-5Vue页面参数传递完成新增文档功能 1.增加页面参数传递 完成新增文档的功能 2.使用路由传递参数ebookId

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -102,10 +102,20 @@
   import {message} from "ant-design-vue";
   import {Tool as tools} from "@/util/tool";
   import {Tool } from "@/util/tool";
+  import {useRoute} from "vue-router";
 
   export default defineComponent({
     name: 'AdminDoc',
     setup() {
+      const route = useRoute();
+      console.log("路由：", route);
+      console.log("route.path：", route.path);
+      console.log("route.query：", route.query);
+      console.log("route.param：", route.params);
+      console.log("route.fullPath：", route.fullPath);
+      console.log("route.name：", route.name);
+      console.log("route.meta：", route.meta);
+
       const param = ref();
       param.value = {};
       const docs = ref();
@@ -248,7 +258,7 @@
        */
       const add = () => {
         modalVisible.value = true;
-        doc.value = {};
+        doc.value = { ebookId: route.query.ebookId};
 
         treeSelectData.value = Tool.copy(level1.value) || [];
 
