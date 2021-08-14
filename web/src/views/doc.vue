d7ca758715711c5b8b30be54c83cdd397d889908
xichuanwushao <avatarwx@163.com>
2021-08-14 22:23:00 +0800

    8-12 点击文档节点时 加载并显示文档内容

--- web/src/views/doc.vue
+++ web/src/views/doc.vue
@@ -13,6 +13,7 @@
           </a-tree>
         </a-col>
         <a-col :span="18">
+          <div :innerHTML="html" ></div>
         </a-col>
       </a-row>
     </a-layout-content>
@@ -24,12 +25,13 @@
   import {message, Modal} from 'ant-design-vue';
   import {Tool as tools} from "@/util/tool";
   import {useRoute} from "vue-router";
+  import {Tool } from "@/util/tool";
   export default defineComponent({
     name: 'AdminDoc',
     setup() {
       const route = useRoute();
       const docs = ref();
-
+      const html = ref();
 
       const level1 = ref(); // 一级分类树，children属性就是二级分类
       level1.value = [];
@@ -51,6 +53,27 @@
         });
       };
 
+
+      /**
+       * 内容查询
+       **/
+      const handleQueryContent = (id : number) => {
+        axios.get("/doc/find-content/"+id).then((response) => {
+          const data = response.data;
+          if(data.success){
+            html.value = data.content;
+          }else {
+            message.error(data.message);
+          }
+        });
+      };
+
+      const onSelect = (selectedKeys: any, info: any)=>{
+        console.log('selected',selectedKeys,info);
+        if(Tool.isNotEmpty(selectedKeys)){
+          handleQueryContent(selectedKeys[0]);
+        }
+      }
       onMounted(() => {
         handleQuery();
       });
@@ -58,6 +81,8 @@
 
       return {
         level1,
+        html,
+        onSelect
       }
     }
   });
