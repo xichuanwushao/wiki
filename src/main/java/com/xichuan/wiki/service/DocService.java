17d61969c2de1c4a6f2bec8a5191eb6236dc241c
xichuanwushao <avatarwx@163.com>
2021-08-14 21:59:22 +0800

    8-12 Bug修复 如果文档内容表没有数据 则查询内容表会出现空指针异常

--- src/main/java/com/xichuan/wiki/service/DocService.java
+++ src/main/java/com/xichuan/wiki/service/DocService.java
@@ -109,6 +109,10 @@ public class DocService {
     }
     public String findContent(Long id) {
         Content content = contentMapper.selectByPrimaryKey(id);
-        return  content.getContent();
+        if(ObjectUtils.isEmpty(content)){
+            return "";
+        }else{
+            return content.getContent();
+        }
     }
 }
