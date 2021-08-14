a69e147d58e21d418a35afe831697021982018cf
xichuanwushao <avatarwx@163.com>
2021-08-14 23:03:51 +0800

    8-12 打开文档页面时 默认选中第一个节点

--- web/src/views/doc.vue
+++ web/src/views/doc.vue
@@ -9,6 +9,7 @@
                   @select="onSelect"
                   :replaceFields="{title: 'name', key: 'id', value: 'id'}"
                   :defaultExpandAll="true"
+                  :defaultSelectedKeys="defaultSelectedKeys"
           >
           </a-tree>
         </a-col>
@@ -33,26 +34,12 @@
       const docs = ref();
       const html = ref();
 
+      const defaultSelectedKeys = ref();
+      defaultSelectedKeys.value = [];
+
       const level1 = ref(); // 一级分类树，children属性就是二级分类
       level1.value = [];
       // level1.value = [];
-
-      /**
-       * 数据查询
-       **/
-      const handleQuery = () => {
-        axios.get("/doc/all/"+route.query.ebookId).then((response) => {
-          const data = response.data;
-          if(data.success){
-            docs.value = data.content;
-            level1.value = [];
-            level1.value = tools.array2Tree(docs.value, 0);
-          }else {
-            message.error(data.message);
-          }
-        });
-      };
-
 
       /**
        * 内容查询
@@ -67,6 +54,32 @@
           }
         });
       };
+      /**
+       * 数据查询
+       **/
+      const handleQuery = () => {
+        axios.get("/doc/all/"+route.query.ebookId).then((response) => {
+          const data = response.data;
+          if(data.success){
+            docs.value = data.content;
+            level1.value = [];
+            level1.value = tools.array2Tree(docs.value, 0);
+
+            //如果有文档 我们采取选中第一个节点
+            if (Tool.isNotEmpty(level1)) {
+              //将该节点设置为选中状态
+              defaultSelectedKeys.value = [level1.value[0].id];
+              //根据这个节点去查找内容
+              handleQueryContent(level1.value[0].id);
+            }
+          }else {
+            message.error(data.message);
+          }
+        });
+      };
+
+
+
 
       const onSelect = (selectedKeys: any, info: any)=>{
         console.log('selected',selectedKeys,info);
@@ -82,7 +95,8 @@
       return {
         level1,
         html,
-        onSelect
+        onSelect,
+        defaultSelectedKeys,
       }
     }
   });
