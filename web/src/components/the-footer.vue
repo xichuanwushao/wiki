8c8182a50e99745fd20044a9c3fcba6c0f17eef8
xichuanwushao <avatarwx@163.com>
2021-08-19 12:04:17 +0800

    9-10 vuex整合sesssession-storage 解决刷新后数据丢失的问题

--- web/src/components/the-footer.vue
+++ web/src/components/the-footer.vue
@@ -1,7 +1,7 @@
 <template>
     <a-layout-footer style="text-align: center">
         Ant Design ©2018 Created by Ant UED 111
-        欢迎：{{user.name}}
+        <span v-show="user.id">欢迎：{{user.name}}</span>
     </a-layout-footer>
 </template>
 <script lang="ts">
