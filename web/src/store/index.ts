d28d32f7f5aaed7fa9b0816aaa0d9dcc135ada0b
xichuanwushao <avatarwx@163.com>
2021-08-19 10:47:19 +0800

    9-10 登录成功处理并集成vuex-2 1.vuex的store集成h5的session-storage 2.刷新后 footer用户信息还有 3.遗留问题 hear的user刷新后没有了

--- web/src/store/index.ts
+++ web/src/store/index.ts
@@ -1,11 +1,19 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
+declare let SessionStorage: any;
+const USER = "USER";
 const store =  createStore({
   state: {
-    user:{}
+    //初始的时候就应该去缓存里面取一下 如果获取不到我们就加上空对象
+    //这样刷新的时候就会加载store 去缓存里面获取一下
+    //获取不到才是一个空对象
+    user:SessionStorage.get("USER") || {}
   },
   mutations: {
     setUser(state, user ){
+      //当登录的时候 除了给store赋值 同时把user放进缓存
+      //当我刷新就去缓存把变量再次取出来
       state.user = user ;
+      SessionStorage.set(USER,user);
     }
   },
   actions: {
