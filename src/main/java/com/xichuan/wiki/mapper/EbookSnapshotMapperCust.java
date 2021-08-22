d7abde908022b58841452918e3d981adb0dfa7d4
xichuanwushao <avatarwx@163.com>
2021-08-22 14:32:53 +0800

    11-7 首页统计数值功能开发 1.后端增加获取统计数值接口

--- src/main/java/com/xichuan/wiki/mapper/EbookSnapshotMapperCust.java
+++ src/main/java/com/xichuan/wiki/mapper/EbookSnapshotMapperCust.java
@@ -1,10 +1,13 @@
 package com.xichuan.wiki.mapper;
 
 import com.xichuan.wiki.domain.Test;
+import com.xichuan.wiki.resp.StatisticResp;
 
 import java.util.List;
 
 public interface EbookSnapshotMapperCust {
 
     public void genSnapshot();
+
+    List<StatisticResp> getStatistic();
 }
--- src/main/java/com/xichuan/wiki/service/EbookSnapshotService.java
+++ src/main/java/com/xichuan/wiki/service/EbookSnapshotService.java
@@ -1,14 +1,22 @@
 package com.xichuan.wiki.service;
 
 import com.xichuan.wiki.mapper.EbookSnapshotMapperCust;
+import com.xichuan.wiki.resp.StatisticResp;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @Service
 public class EbookSnapshotService {
     @Resource
     private EbookSnapshotMapperCust ebookSnapshotMapperCust;
+    /**
+     * 获取首页数值数据：总阅读数、总点赞数、今日阅读数、今日点赞数、今日预计阅读数、今日预计阅读增长
+     */
+    public List<StatisticResp> getStatistic() {
+        return ebookSnapshotMapperCust.getStatistic();
+    }
 
     public void genSnapshot() {
          ebookSnapshotMapperCust.genSnapshot();
